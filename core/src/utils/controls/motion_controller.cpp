9e6aaae23d8f91b98b02cf700a6a9a1356730561
Ryan McGee <ramtech51@comcast.net>
2024-01-16 22:03:16 -0500

    git subrepo commit (merge) core
    subrepo:
      subdir:   "core"
      merged:   "e2cb990"
    upstream:
      origin:   "git@github.com:RIT-VEX-U/Core.git"
      branch:   "main"
      commit:   "d1921ad"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

--- core/src/utils/controls/motion_controller.cpp
+++ core/src/utils/controls/motion_controller.cpp
@@ -1,4 +1,5 @@
 #include "../core/include/utils/controls/motion_controller.h"
+#include "../core/include/subsystems/screen.h"
 #include "../core/include/utils/math_util.h"
 #include <vector>
 
@@ -6,15 +7,14 @@
  * @brief Construct a new Motion Controller object
  *
  * @param config The definition of how the robot is able to move
- *    max_v Maximum velocity the movement is capable of
- *    accel Acceleration / deceleration of the movement
- *    pid_cfg Definitions of kP, kI, and kD
- *    ff_cfg Definitions of kS, kV, and kA
+ * max_v Maximum velocity the movement is capable of
+ * accel Acceleration / deceleration of the movement
+ * pid_cfg Definitions of kP, kI, and kD
+ * ff_cfg Definitions of kS, kV, and kA
  */
 MotionController::MotionController(m_profile_cfg_t &config)
-    : config(config), pid(config.pid_cfg), ff(config.ff_cfg), profile(config.max_v, config.accel)
-{
-}
+    : config(config), pid(config.pid_cfg), ff(config.ff_cfg),
+      profile(config.max_v, config.accel) {}
 
 /**
  * @brief Initialize the motion profile for a new movement
@@ -24,8 +24,8 @@ MotionController::MotionController(m_profile_cfg_t &config)
  * @param start_vel Movement starting velocity
  * @param end_vel Movement ending velocity
  */
-void MotionController::init(double start_pt, double end_pt, double start_vel, double end_vel)
-{
+void MotionController::init(double start_pt, double end_pt, double start_vel,
+                            double end_vel) {
     profile.set_endpts(start_pt, end_pt);
     profile.set_vel_endpts(start_vel, end_vel);
     pid.reset();
@@ -40,8 +40,7 @@ void MotionController::init(double start_pt, double end_pt, double start_vel, do
  * @param sensor_val Value from the sensor
  * @return the motor input generated from the motion profile
  */
-double MotionController::update(double sensor_val)
-{
+double MotionController::update(double sensor_val) {
     cur_motion = profile.calculate(tmr.time(timeUnits::sec), sensor_val);
     pid.set_target(cur_motion.pos);
     pid.update(sensor_val);
@@ -50,8 +49,9 @@ double MotionController::update(double sensor_val)
 
     out = pid.get() + ff.calculate(cur_motion.vel, cur_motion.accel, pid.get());
 
-    if (lower_limit != upper_limit)
+    if (lower_limit != upper_limit) {
         out = clamp(out, lower_limit, upper_limit);
+    }
 
     return out;
 }
@@ -59,19 +59,16 @@ double MotionController::update(double sensor_val)
 /**
  * @return the last saved result from the feedback controller
  */
-double MotionController::get()
-{
-    return out;
-}
+double MotionController::get() { return out; }
 
 /**
- * Clamp the upper and lower limits of the output. If both are 0, no limits should be applied.
+ * Clamp the upper and lower limits of the output. If both are 0, no limits
+ * should be applied.
  *
  * @param lower Upper limit
  * @param upper Lower limit
  */
-void MotionController::set_limits(double lower, double upper)
-{
+void MotionController::set_limits(double lower, double upper) {
     lower_limit = lower;
     upper_limit = upper;
 }
@@ -80,30 +77,29 @@ void MotionController::set_limits(double lower, double upper)
  * @return Whether or not the movement has finished, and the PID
  * confirms it is on target
  */
-bool MotionController::is_on_target()
-{
-    return (tmr.time(timeUnits::sec) > profile.get_movement_time()) && pid.is_on_target() && fabs(end_pt - current_pos) < pid.config.deadband;
+bool MotionController::is_on_target() {
+    return (tmr.time(timeUnits::sec) > profile.get_movement_time()) &&
+           pid.is_on_target() &&
+           fabs(end_pt - current_pos) < pid.config.deadband;
 }
 
 /**
  * @return The current postion, velocity and acceleration setpoints
  */
-motion_t MotionController::get_motion()
-{
-    return cur_motion;
-}
+motion_t MotionController::get_motion() const { return cur_motion; }
 
 /**
- * This method attempts to characterize the robot's drivetrain and automatically tune the feedforward.
- * It does this by first calculating the kS (voltage to overcome static friction) by slowly increasing
- * the voltage until it moves.
+ * This method attempts to characterize the robot's drivetrain and automatically
+ * tune the feedforward. It does this by first calculating the kS (voltage to
+ * overcome static friction) by slowly increasing the voltage until it moves.
  *
- * Next is kV (voltage to sustain a certain velocity), where the robot will record it's steady-state velocity
- * at 'pct' speed.
+ * Next is kV (voltage to sustain a certain velocity), where the robot will
+ * record it's steady-state velocity at 'pct' speed.
  *
- * Finally, kA (voltage needed to accelerate by a certain rate), where the robot will record the entire movement's
- * velocity and acceleration, record a plot of [X=(pct-kV*V-kS), Y=(Acceleration)] along the movement,
- * and since kA*Accel = pct-kV*V-kS, the reciprocal of the linear regression is the kA value.
+ * Finally, kA (voltage needed to accelerate by a certain rate), where the robot
+ * will record the entire movement's velocity and acceleration, record a plot of
+ * [X=(pct-kV*V-kS), Y=(Acceleration)] along the movement, and since kA*Accel =
+ * pct-kV*V-kS, the reciprocal of the linear regression is the kA value.
  *
  * @param drive The tankdrive to operate on
  * @param odometry The robot's odometry subsystem
@@ -111,8 +107,9 @@ motion_t MotionController::get_motion()
  * @param duration Amount of time the robot should be moving for the test
  * @return A tuned feedforward object
  */
-FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, OdometryTank &odometry, double pct, double duration)
-{
+FeedForward::ff_config_t
+MotionController::tune_feedforward(TankDrive &drive, OdometryTank &odometry,
+                                   double pct, double duration) {
     FeedForward::ff_config_t out = {};
 
     pose_t start_pos = odometry.get_position();
@@ -120,8 +117,7 @@ FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, Od
     // ========== kS Tuning =========
     // Start at 0 and slowly increase the power until the robot starts moving
     double power = 0;
-    while (odometry.pos_diff(start_pos, odometry.get_position()) < 0.05)
-    {
+    while (odometry.pos_diff(start_pos, odometry.get_position()) < 0.05) {
         drive.drive_tank(power, power, 1);
         power += 0.001;
         vexDelay(100);
@@ -142,11 +138,11 @@ FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, Od
     MovingAverage vel_ma(3);
     MovingAverage accel_ma(3);
 
-    // Move the robot forward at a fixed percentage for X seconds while taking velocity and accel measurements
+    // Move the robot forward at a fixed percentage for X seconds while taking
+    // velocity and accel measurements
 
     drive.drive_tank(pct, pct, 1);
-    do
-    {
+    do {
         time = tmr.time(sec);
 
         vel_ma.add_entry(odometry.get_speed());
@@ -155,23 +151,22 @@ FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, Od
         double speed = vel_ma.get_value();
         double accel = accel_ma.get_value();
         // For kV:
-        if (speed > max_speed)
-        {
+        if (speed > max_speed) {
             max_speed = speed;
         }
-        if (accel > max_accel){
+        if (accel > max_accel) {
             max_accel = accel;
         }
 
         // For kA:
         // Filter out the acceleration dampening due to motor inductance
-        if (time > 0.25)
-        {
+        if (time > 0.25) {
             vel_data_points.push_back(std::pair<double, double>(time, speed));
             accel_data_points.push_back(std::pair<double, double>(time, accel));
         }
 
-        // Theoretical polling rate = 100hz (it won't be that much, cause, y'know, vex.)
+        // Theoretical polling rate = 100hz (it won't be that much, cause,
+        // y'know, vex.)
         vexDelay(10);
     } while (time < duration);
 
@@ -185,11 +180,12 @@ FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, Od
 
     // Calculate kA (volts/12 per unit per second^2)
     std::vector<std::pair<double, double>> accel_per_pct;
-    for (int i = 0; i < vel_data_points.size(); i++)
-    {
+    for (int i = 0; i < vel_data_points.size(); i++) {
         accel_per_pct.push_back(std::pair<double, double>(
-            pct - out.kS - (vel_data_points[i].second * out.kV), // Acceleration-causing percent (X variable)
-            accel_data_points[i].second                          // Measured acceleration (Y variable)
+            pct - out.kS -
+                (vel_data_points[i].second *
+                 out.kV), // Acceleration-causing percent (X variable)
+            accel_data_points[i].second // Measured acceleration (Y variable)
             ));
     }
 
@@ -198,4 +194,56 @@ FeedForward::ff_config_t MotionController::tune_feedforward(TankDrive &drive, Od
     out.kA = 1.0 / regres_slope;
 
     return out;
+}
+
+class MotionControllerPage : public screen::Page {
+  public:
+    MotionControllerPage(const MotionController &mc) : mc(mc) {}
+
+    void update(bool was_pressed, int x, int y) override {}
+    void draw(vex::brain::lcd &screen, bool first_draw,
+              unsigned int frame_number) {
+        const motion_t mot = mc.get_motion();
+
+        // Text - top right
+        screen.printAt(240, 20, "pos: %.2fin", mot.pos);
+        screen.printAt(240, 40, "vel: %.2fin", mot.vel);
+        screen.printAt(240, 60, "acc: %.2fin", mot.accel);
+        screen.printAt(240, 80, "%.2fs of %.2fs", mc.tmr.value(),
+                       mc.profile.get_movement_time());
+
+        // Trapezoid - bottom right
+        const int32_t trap_width = 200;
+        const int32_t trap_height = 98;
+        const double seconds = mc.tmr.value();
+        const double full_time = mc.profile.get_movement_time();
+        const double x_pct = seconds / full_time;
+        const double y_pct = mot.vel / mc.profile.get_max_v();
+        const int32_t x_pos = (int32_t)(x_pct * trap_width) + 240;
+        const int32_t y_pos = (int32_t)(-y_pct * trap_height) + 100;
+        const int32_t max_vel_y = 120;
+        const int32_t end_acc_x = 260;
+        const int32_t start_dec_x = 420;
+
+        // trapezoid
+        screen.setPenColor(vex::red);
+        // accelerating
+        screen.drawLine(240, 200, end_acc_x, max_vel_y);
+        // staying
+        screen.drawLine(end_acc_x, max_vel_y, start_dec_x, max_vel_y);
+        // decellerating
+        screen.drawLine(440, 200, start_dec_x, max_vel_y);
+
+        // dot where we are
+        screen.setFillColor(vex::red);
+        screen.setPenColor(vex::white);
+        screen.drawCircle(x_pos, y_pos, 3);
+    }
+
+  private:
+    const MotionController &mc;
+};
+
+screen::Page *MotionController::Page() {
+    return new MotionControllerPage(*this);
 }
